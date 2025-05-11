dac9263afa96b9a870072fb7e327ed0dbf418caf
elawrey <e.lawrey@aims.gov.au>
2025-05-11 21:48:45 +1000

    Added cleanup and prep

--- 01-compare-weights.sh
+++ 01-compare-weights.sh
@@ -7,7 +7,7 @@
 # and recall.
 
 # Define the dataset path as a variable
-DATASET_PATH=~/Documents/2025/GIS/AU_AIMS_S2-comp
+IMAGERY_PATH=~/Documents/2025/GIS/AU_AIMS_S2-comp
 
 GEOTIFF_DIR=working/rocky-reefs-geotiffs
 
@@ -15,11 +15,26 @@ PARALLEL=15
 
 weights=(1.0 1.5 2.0 2.5 3.0)
 
+# Clean out any previous training data
+rm -rf working/training-data
+
+python 02-extract-training-data.py --imagery-path "$IMAGERY_PATH"
+
+python 04-train-random-forest.py 
+
+# Make sure we are not reusing any values from a previous run.
+rm -rf working/06-multi_w* working/06-binary_w* working/07-multi_w* working/07-binary_w*
+
+
 # Loop through each weight
 for weight in "${weights[@]}"; do
   echo "-------------- MULTI CLASS WEIGHT $weight ------------------"
+  # Clear out the geotiff predictions, so they are not accidentally reused
+  rm -rf "$GEOTIFF_DIR"
+
+
   python 06-classify-rocky-reefs.py \
-    --dataset-path "$DATASET_PATH" \
+    --dataset-path "$IMAGERY_PATH" \
     --geotiff-dir "$GEOTIFF_DIR" \
     --regions NorthernAU,GBR \
     --parallel "$PARALLEL" \
@@ -27,14 +42,14 @@ for weight in "${weights[@]}"; do
 
   python 07-merge-scenes.py --weight "$weight"
 
-  # Clear out the geotiff predictions, so they are not accidentally reused
-  rm -rf "$GEOTIFF_DIR"
+  
 done
 
 for weight in "${weights[@]}"; do
   echo "-------------- BINARY CLASS WEIGHT $weight ------------------"
+  rm -rf "$GEOTIFF_DIR"
   python 06-classify-rocky-reefs.py \
-    --dataset-path "$DATASET_PATH" \
+    --dataset-path "$IMAGERY_PATH" \
     --geotiff-dir "$GEOTIFF_DIR" \
     --regions NorthernAU,GBR \
     --parallel "$PARALLEL" \
@@ -42,5 +57,5 @@ for weight in "${weights[@]}"; do
     --binary-classifier
 
   python 07-merge-scenes.py --weight "$weight" --binary-classifier
-  rm -rf "$GEOTIFF_DIR"
+  
 done
